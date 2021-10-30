afedb39d33ceab142f4f11d5c8b4cf9d10cf744c
Samuel Parra <samuel.parra@gmail.com>
2021-10-30 20:40:23 -0300

    add models

---
+++ db/migrate/20211030233427_create_buildings.rb
@@ -0,0 +1,11 @@
+class CreateBuildings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :buildings do |t|
+      t.string :name
+      t.string :city
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
