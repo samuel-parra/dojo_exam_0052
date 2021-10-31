0b0f781f6986811eb1981865640e1852b997f34a
Samuel Parra <samuel.parra@gmail.com>
2021-10-31 18:38:01 -0300

    add base of aparment and new

---
+++ app/controllers/apartments_controller.rb
@@ -0,0 +1,32 @@
+class ApartmentsController < ApplicationController
+  def index
+    @apartments = Apartment.all.order(:building_id)
+  end
+
+  def new
+    @apartment = Apartment.new
+    @buildings = Building.all.order(:id)
+  end
+
+  def show
+    @apartment = Apartment.find(params[:id])
+  end
+
+  def create
+    @apartment = Apartment.new(aparment_params)
+    if @apartment.save
+      redirect_to apartments_path
+    else
+      render("new")
+    end    
+  end
+
+  def delete
+  end
+
+  private
+
+  def aparment_params
+    params.require(:apartment).permit(:number,:building_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :buildings
+  resources :apartments
 
 end
