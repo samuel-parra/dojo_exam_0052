47d602dbf1535ae58a7814f6cd7e076c579d0096
Samuel Parra <samuel.parra@gmail.com>
2021-10-30 22:27:10 -0300

    add seed for test proposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times {
+    |i| puts "#{i}"
+    Building.create(name: "Edificio #{i+1}", city: "Concepción", address: "Calle principal numero #{100 + i}")
+}
+
+10.times {
+    |i| puts "#{i}"
+    nofapp =  rand(1..10)
+    puts "number of apparments #{nofapp}"
+    nofapp.times {
+        |j| puts "#{j}"
+        appnum = Building.find(j+1)
+        Apartment.create(number: (j+1), building: appnum)
+    }
+}
